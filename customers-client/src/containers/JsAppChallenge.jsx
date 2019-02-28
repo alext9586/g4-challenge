f8642173cff1bfeeb99044273a151fdbc5b7e4db
alext9586 <alext9586@users.noreply.github.com>
2019-02-27 19:37:52 -0800

    Moved search bar to own component

--- customers-client/src/containers/JsAppChallenge.jsx
+++ customers-client/src/containers/JsAppChallenge.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CustomerTableReadOnly from '../components/CustomerTableReadOnly';
+import SearchBar from '../components/SearchBar';
 
 class JsAppChallenge extends Component {
   constructor() {
@@ -7,13 +8,10 @@ class JsAppChallenge extends Component {
 
     this.state = {
       customers: [],
-      searchTerms: [],
+      searchTerms: "",
       maxResults: 20
     }
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSearch = this.handleSearch.bind(this);
-    this.handleClear = this.handleClear.bind(this);
     this.increaseSearchResults = this.increaseSearchResults.bind(this);
   }
 
@@ -30,28 +28,7 @@ class JsAppChallenge extends Component {
       });
   }
 
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.value;
-
-    this.setState({
-      searchTerms: value
-    });
-  }
-
-  handleClear(event) {
-    event.preventDefault();
-    this.setState({
-      searchTerms: ""
-    })
-  }
-
-  handleSearch(event) {
-    event.preventDefault();
-    console.log("Submit")
-  }
-
-  increaseSearchResults(event) {
+  increaseSearchResults() {
     this.setState({
       maxResults: this.state.maxResults + 20
     })
@@ -67,39 +44,20 @@ class JsAppChallenge extends Component {
 
         { customers.length > 0 ? (
           <div>
-            <form className="text-left" onSubmit={this.handleSearch}>
-              <div className="form-row">
-                <div className="col">
-                  <input
-                    name="searchTerms"
-                    type="text"
-                    className="form-control form-control-sm"
-                    onChange={this.handleInputChange}
-                    value={searchTerms} />
-                </div>
-                <div className="col">
-                  <input
-                    type="button"
-                    className="btn btn-danger"
-                    onClick={this.handleClear}
-                    value="Clear"/>
-                    
-                  <input
-                    type="submit"
-                    className="btn btn-primary"
-                    value="Search"/>
-                </div>
-              </div>
-            </form>
+            <SearchBar
+              searchTerms={searchTerms}
+              searchClick={(e) => { console.log(e) }} />
 
             <CustomerTableReadOnly
               customers={truncatedData} />
 
-            <input
-              type="button"
-              className="btn btn-block btn-primary"
-              value="Load 20 more..."
-              onClick={this.increaseSearchResults}/>
+            { maxResults < customers.length ? 
+              <input
+                type="button"
+                className="btn btn-block btn-primary"
+                value="Load 20 more..."
+                onClick={this.increaseSearchResults}/>
+            : null }
           </div>
         ) : (<h2>Loading...</h2>)}
       </div>
